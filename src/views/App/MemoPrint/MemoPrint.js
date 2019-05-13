b09c49447bd619425d30a473910f752be18449e3
nari19 <kazuki7793996@gmail.com>
2019-05-13 13:40:53 +0900

    html2canvas

--- src/views/App/MemoPrint/MemoPrint.js
+++ src/views/App/MemoPrint/MemoPrint.js
@@ -5,36 +5,36 @@ import { Row, Col } from 'reactstrap';
 import { rgbToHex } from '@coreui/coreui/dist/js/coreui-utilities';
 import html2canvas from "html2canvas";
 
-function getDisplayImage(){
-  //html2canvas実行
-  html2canvas(document.getElementById("target")).then(function(canvas) {
-      downloadImage(canvas.toDataURL());
-  });
-}
-  
-function downloadImage (data) {
-  var fname ="download.png";
-  var encdata= atob(data.replace(/^.*,/, ''));
-  var outdata = new Uint8Array(encdata.length);
-
-  for (var i = 0; i < encdata.length; i++) {
-      outdata[i] = encdata.charCodeAt(i);
-  }
-  
-  var blob = new Blob([outdata], ["image/png"]);
-  if (window.navigator.msSaveBlob) {
-      //IE用
-      window.navigator.msSaveOrOpenBlob(blob, fname);
-  } else {
-      //それ以外？
-      document.getElementById("getImage").href=data; //base64そのまま設定
-      document.getElementById("getImage").download=fname; //ダウンロードファイル名設定
-      document.getElementById("getImage").click(); //自動クリック
-  }
-}
-
 
 class MemoPrint extends Component {
+  getDisplayImage() {
+    //html2canvas実行
+    html2canvas(document.getElementById("target")).then(function(canvas) {
+        this.downloadImage(canvas.toDataURL());
+    });
+  }
+    
+  downloadImage (data) {
+    const fname ="download.png";
+    const encdata= atob(data.replace(/^.*,/, ''));
+    const outdata = new Uint8Array(encdata.length);
+  
+    for (const i = 0; i < encdata.length; i++) {
+        outdata[i] = encdata.charCodeAt(i);
+    }
+    
+    const blob = new Blob([outdata], ["image/png"]);
+    if (window.navigator.msSaveBlob) {
+        //IE用
+        window.navigator.msSaveOrOpenBlob(blob, fname);
+    } else {
+        //それ以外？
+        document.getElementById("getImage").href=data; //base64そのまま設定
+        document.getElementById("getImage").download=fname; //ダウンロードファイル名設定
+        document.getElementById("getImage").click(); //自動クリック
+    }
+  }
+
   render() {
     return (
       <div className="animated fadeIn">
@@ -45,8 +45,8 @@ class MemoPrint extends Component {
           <div className="card-body" id="target">
             <p>aaaaaaaaaaaa</p>
 
-            <div id="downloadImageButton" class="btn btn-primary btn-lg" onclick="getDisplayImage();" >Sava image</div>
-            <a id="getImage" href=""  style="display:none;"  download="image.png">画像保存</a>
+            <div id="downloadImageButton" class="btn btn-primary btn-lg" onClick={this.getDisplayImage()} >Sava image</div>
+            <a id="getImage" href=""  download="image.png">画像保存</a>
           </div>
         </div>
       </div>
