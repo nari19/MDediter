e076b556c3d56ae7e05dae025eda825b6a0ab890
nari19 <kazuki7793996@gmail.com>
2019-06-04 10:52:52 +0900

    top margin

--- src/Viewer/DomHTML.js
+++ src/Viewer/DomHTML.js
@@ -42,7 +42,7 @@ export default class DomHTML extends PureComponent {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${width} ${height}`} width="100%" >
         <style>{ gtCss }</style>
         <g dangerouslySetInnerHTML={{
-          __html: `<foreignObject x="0" y="50" width="100%" height="100%" class="markdown-body">${ReactDOMServer.renderToStaticMarkup(this.renderInnerHtml())}</foreignObject>`
+          __html: `<foreignObject x="0" y="0" width="100%" height="100%" class="markdown-body">${ReactDOMServer.renderToStaticMarkup(this.renderInnerHtml())}</foreignObject>`
         }} />
       </svg>
     );
